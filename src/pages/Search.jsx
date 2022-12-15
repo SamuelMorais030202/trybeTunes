fa1f7909726c1bcd28141c22054b7ea398350a92
Samuel Morais030202 <moraissamuel009@gmail.com>
2022-12-15 14:46:37 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,7 +2,19 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  state = {
+    searchUser: '',
+  };
+
+  handleSearch = ({ target }) => {
+    this.setState({
+      searchUser: target.value,
+    });
+  };
+
   render() {
+    const { searchUser } = this.state;
+    const disabled = searchUser.length >= 2;
     return (
       <div data-testid="page-search">
         <Header />
@@ -10,10 +22,12 @@ class Search extends React.Component {
           <input
             type="text"
             data-testid="search-artist-input"
+            onChange={ this.handleSearch }
           />
           <button
             type="button"
             data-testid="search-artist-button"
+            disabled={ !disabled }
           >
             Pesquisar
           </button>
