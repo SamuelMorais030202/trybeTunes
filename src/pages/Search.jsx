baee12c1f8c981d3e13088ac5bf74f236e7b3646
Samuel Morais030202 <moraissamuel009@gmail.com>
2022-12-15 17:22:03 -0300

    Requisito 6

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import Carregando from './Carregando';
 
 class Search extends React.Component {
   state = {
+    name: '',
     searchUser: '',
+    verification: true,
+    responseAPI: false,
+    API: [],
+    failureAPI: false,
+  };
+
+  submitButton = async () => {
+    const { searchUser } = this.state;
+    const search = searchUser;
+    this.setState({
+      name: search,
+      searchUser: '',
+      verification: false,
+    });
+    const APIresponse = await searchAlbumsAPI(search);
+    this.setState({
+      verification: true,
+      responseAPI: true,
+      API: APIresponse,
+      failureAPI: APIresponse.length === 0,
+    });
   };
 
   handleSearch = ({ target }) => {
@@ -13,25 +38,59 @@ class Search extends React.Component {
   };
 
   render() {
-    const { searchUser } = this.state;
+    const { searchUser, verification, failureAPI, name, API, responseAPI } = this.state;
     const disabled = searchUser.length >= 2;
+    console.log(responseAPI);
+    const response = `Resultado de álbuns de: ${name}`;
     return (
       <div data-testid="page-search">
         <Header />
-        <div>
-          <input
-            type="text"
-            data-testid="search-artist-input"
-            onChange={ this.handleSearch }
-          />
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ !disabled }
-          >
-            Pesquisar
-          </button>
-        </div>
+        {
+          verification
+            ? (
+              <div>
+                <input
+                  value={ searchUser }
+                  type="text"
+                  data-testid="search-artist-input"
+                  onChange={ this.handleSearch }
+                />
+                <button
+                  type="button"
+                  data-testid="search-artist-button"
+                  disabled={ !disabled }
+                  onClick={ this.submitButton }
+                >
+                  Pesquisar
+                </button>
+              </div>
+            ) : <Carregando />
+        }
+        {
+          API.length > 0 && (
+            <>
+              <h2>{response}</h2>
+              <div>
+                {
+                  API.map((album) => (
+                    <Link
+                      to={ `/album/${album.collectionId}` }
+                      key={ album.artistName }
+                      data-testid={ `link-to-album-${album.collectionId}` }
+                    >
+                      <div key={ album.artistName }>
+                        <img src={ album.artworkUrl100 } alt="Imagem album" />
+                        <h4>{album.collectionName}</h4>
+                        <p>{album.artistName}</p>
+                      </div>
+                    </Link>
+                  ))
+                }
+              </div>
+            </>
+          )
+        }
+        {failureAPI && responseAPI && (<h2>Nenhum álbum foi encontrado</h2>) }
       </div>
     );
   }
