c6f16505a5822b1219edadf2ec40721b1f23b68c
Samuel Morais030202 <moraissamuel009@gmail.com>
2023-01-14 10:13:44 -0300

    Finalizando requisito 12

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,12 +1,79 @@
 import React from 'react';
 import Header from '../components/Header';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
+import Carregando from './Carregando';
+// import MusicCard from '../components/MusicCard';
 
 class Favorites extends React.Component {
+  state = {
+    loading: true,
+    musicsFavorits: [],
+    check: true,
+  };
+
+  componentDidMount() {
+    this.loadingFavoritsSongs();
+  }
+
+  disfavoringMusic = async (music) => {
+    this.setState({
+      loading: true,
+    });
+    await removeSong(music);
+    const updatingFavoriteSongs = await getFavoriteSongs();
+    this.setState({
+      check: false,
+      musicsFavorits: updatingFavoriteSongs,
+      loading: false,
+    });
+  };
+
+  loadingFavoritsSongs = async () => {
+    const musics = await getFavoriteSongs();
+    this.setState({
+      loading: false,
+      musicsFavorits: musics,
+    });
+  };
+
   render() {
+    const { loading, musicsFavorits, check } = this.state;
+    console.log(musicsFavorits);
     return (
       <div data-testid="page-favorites">
         <Header />
-        <p>Teste</p>
+        <div>
+          {
+            loading
+              ? <Carregando />
+              : (
+                musicsFavorits.map((music) => (
+                  <div key={ music.trackName }>
+                    <h5>{ music.trackName }</h5>
+                    <audio
+                      src={ music.previewUrl }
+                      data-testid="audio-component"
+                      controls
+                    >
+                      <track kind="captions" />
+                      O seu navegador não suporta o elemento
+                      <code>audio</code>
+                    </audio>
+                    <label
+                      htmlFor={ `checkbox-music-${music.trackId}` }
+                    >
+                      <input
+                        type="checkbox"
+                        checked={ check }
+                        name="checked"
+                        onChange={ () => this.disfavoringMusic(music) }
+                      />
+                    </label>
+                  </div>
+                ))
+              )
+          }
+        </div>
       </div>
     );
   }
